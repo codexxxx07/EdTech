aa1bbab74ab185f87793f8ba684c43c953bf0506
codexxxx07 <ikrish21052007@gmail.com>
2026-06-18 03:28:22 +0530

    Deleted Old Structure

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: ["./index.html", "./script.js"],
-  darkMode: "class",
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
